16797f1db049d042deb2f04a51565f5c1a9b5d1b
Livia_Tassel <3459465562@qq.com>
2025-11-04 15:52:08 +0800

    1040.div.2.0

--- CF/1040.div.2/C.cpp
+++ CF/1040.div.2/C.cpp
@@ -1,4 +1,80 @@
-for (ll i = 0; i < n; i++)
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <list>
+#include <set>
+#include <stack>
+#include <map>
+#include <numeric>
+#include <unordered_map>
+#define ll long long
+using namespace std;
+using vl = vector<ll>;
+using vp = vector<pair<ll, ll>>;
+const ll maxn = 6e3 + 5;
+ll root[maxn];
+ll n;
+
+void init()
 {
-    root[i] = i;
+    for (ll i = 1; i <= 2 * n; i++)
+    {
+        root[i] = i;
+    }
+}
+
+ll find(ll x)
+{
+    return root[x] != x ? root[x] = find(root[x]) : x;
+}
+
+void un(ll x, ll y)
+{
+    ll fx = find(x), fy = find(y);
+    if (fx != fy)
+    {
+        root[fy] = fx;
+    }
+}
+
+void solve()
+{
+    cin >> n;
+    init();
+    vl a(n), b(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i] >> b[i];
+    }
+
+    vl ans;
+    ll cnt = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        if (find(a[i]) != find(b[i]))
+        {
+            un(a[i], b[i]);
+            ans.push_back(i + 1);
+            cnt++;
+        }
+    }
+    cout << cnt << endl;
+    for (ll i = 0; i < cnt; i++)
+    {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+}
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
 }
--- CF/1040.div.2/D.cpp
+++ CF/1040.div.2/D.cpp
@@ -15,9 +15,39 @@ using vl = vector<ll>;
 
 void solve()
 {
-    ll n;
+    ll n, ans = 0;
     cin >> n;
-    vl a(n);
+    vl p(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> p[i];
+    }
+
+    // x_i = 0, p_i; x_i = 1, 2*n - p_i
+    // C = Inv(P) + sum_{1, n}(x_i * (sum_{j > i}[p_i < p_j] - sum_{j < i}[p_j > p_i]))
+    // for each i, x_i * (R_i - L_i), L_i = sum_{j < i}[p_j > p_i], R_i = sum_{j > i}[p_i < p_j]
+    // R_i - L_i >= 0, then x_i = 0
+    // R_i - L_i < 0, then x_i = 1
+    for (ll i = 0; i < n; i++)
+    {
+        ll l = 0, r = 0;
+        for (ll j = 0; j < i; j++)
+        {
+            if (p[j] > p[i])
+            {
+                l++;
+            }
+        }
+        for (ll j = i + 1; j < n; j++)
+        {
+            if (p[j] > p[i])
+            {
+                r++;
+            }
+        }
+        ans += min(l, r);
+    }
+    cout << ans << '\n';
 }
 signed main()
 {
